42f369bdc69e65e52d8ac89f681b636950f03b87
Waldek <waldek04@o2.pl>
2020-08-29 19:53:51 +0200

    dodanie ładowanie z pliku do tablicy tasks oraz listowanie tablicy tasks

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,5 +1,12 @@
 package pl.coderslab;
 
+import com.sun.jdi.connect.Connector;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class TaskManager {
@@ -11,30 +18,36 @@ public class TaskManager {
     public static void main(String[] args) {
         String selectedOption = "";
 
-        while(true) {
+        if (!loadDBFromFile()) {
+            System.out.println("Problem z Odczytem pliku bazy.");
+            System.exit(0);
+        }
+
+        while (true) {
             menu();
             selectedOption = getOption();
             switch (selectedOption) {
                 case "add":
-                    System.out.println(ConsoleColors.CYAN + "ADD"+ConsoleColors.RESET);
+                    addOption();
                     break;
                 case "remove":
-                    System.out.println(ConsoleColors.CYAN + "REMOVE"+ConsoleColors.RESET);
+                    removeOption();
                     break;
                 case "list":
-                    System.out.println(ConsoleColors.CYAN + "LIST"+ConsoleColors.RESET);
+                    listOption();
                     break;
                 case "exit":
-                    System.out.println(ConsoleColors.CYAN + "EXIT"+ConsoleColors.RESET);
-                    return;
+                    exitOption();
+                    //return;
+                    System.exit(0);
                 default:
-                    System.out.println(ConsoleColors.RED + "Please select an appropriate option!!"+ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.RED + "Please select an appropriate option!!" + ConsoleColors.RESET);
             }
         }
     }
 
     private static void menu() {
-        System.out.println(ConsoleColors.BLUE + "Please select an option:"+ConsoleColors.RESET );
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
         for (String menuOpt : MENU_OPTS) {
             System.out.println(menuOpt);
         }
@@ -44,4 +57,58 @@ public class TaskManager {
         Scanner scan = new Scanner(System.in);
         return scan.next();
     }
+
+    private static boolean loadDBFromFile() {
+        Path path = Paths.get(DB_FILE_NAME);
+        if (!Files.exists(path)) {
+            return false;
+        }
+        try {
+            List<String> tasksLines = Files.readAllLines(path);
+            int numberOftasks = tasksLines.size();
+            System.out.println("taskSize = " + numberOftasks);
+
+            tasks = new String[numberOftasks][];
+
+            /*for (String line : tasksLines) {
+                System.out.println(line);
+            }*/
+            for (int i = 0; i < tasks.length; i++) {
+                tasks[i] = tasksLines.get(i).split(",");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return true;
+    }
+
+    private static void addOption() {
+        System.out.println(ConsoleColors.CYAN + "ADD" + ConsoleColors.RESET);
+    }
+
+    private static void removeOption() {
+        System.out.println(ConsoleColors.CYAN + "REMOVE" + ConsoleColors.RESET);
+    }
+
+    private static void listOption() {
+        System.out.println(ConsoleColors.CYAN + "LIST" + ConsoleColors.RESET);
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.printf("%2d: ", i);
+            for (int j = 0; j < tasks[i].length; j++) {
+                if (j == tasks[i].length - 1) {
+                    System.out.println(ConsoleColors.RED + tasks[i][j] + ConsoleColors.RESET);
+                } else {
+                    System.out.print(tasks[i][j]);
+                    if (j < tasks[i].length-1) {
+                        System.out.print("\t");
+                    }
+                }
+            }
+        }
+    }
+
+    private static void exitOption() {
+        System.out.println(ConsoleColors.CYAN + "EXIT" + ConsoleColors.RESET);
+    }
+
 }
